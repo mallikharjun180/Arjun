0adbe0e7b7798728215f388edc2782b76522cd43
mallikharjun <mallikharjun180@gmail.com>
2019-09-22 17:45:52 +0530

    selenium second time

--- src/com/pack/git/Test26.java
+++ src/com/pack/git/Test26.java
@@ -5,6 +5,11 @@ public class Test26 {
 	public static void main(String[] args)
 	{
 		System.out.println("hello GitHub");
+		Test26.m1();
+	}
+	static void m1()
+	{
+		System.out.println("git");
 	}
 
 }
